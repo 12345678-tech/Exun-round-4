c2be8da165603d35661a986dfdb299965f913475
AyaanJ <66999423+12345678-tech@users.noreply.github.com>
2022-07-08 00:59:26 +0530

    Add files via upload
    node_modules

--- search.js
+++
@@ -1,9 +0,0 @@
-const toBeSearched = process.argv.slice(2);
-
-if(!toBeSearched.length){
-	console.log("Please provide string to search");
-}
-else {
-	const sampleText = 'Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser, which was designed to build scalable network applications.'
-	console.log(sampleText.includes(toBeSearched))
-}
